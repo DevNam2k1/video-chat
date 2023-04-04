1cb0dafca9daaf6a6b2a540cf9fe78c66275c420
nam.nghiem <nam.nghiem@savvycomsoftware.com>
2023-04-04 15:54:02 +0700

    First commit

---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Home = () => {
+  const [RoomCode, setRoomCode] = useState("");
+  const navigate = useNavigate();
+
+  const submitCode = (e) => {
+    e.preventDefault();
+    navigate(`/room/${RoomCode}`);
+  };
+    return (
+    <div>
+        <form action='' 
+        onSubmit={submitCode}
+        className='flex flex-col items-center justify-center'>
+        <label htmlFor='' className='font-bold text-[32px] pt-12'> Enter The Room Code</label>
+        <input 
+        type='text' 
+        required 
+        placeholder='Enter Room Code' 
+        value={RoomCode}
+        onChange={(e) => setRoomCode(e.target.value)} 
+        className='bg-blue-500
+        placeholder:text-white py-2 w-[16rem]
+        rounded-full pl-8 mt-4'/>
+        <button type='submit'
+        className='bg-orange-500 rounded-[0.5rem] 
+        mt-4 py-1.5 px-4 font-bold text-white'>Enter Room</button>
+        </form>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Room.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+const Room = () => {
+  const { roomID } = useParams();
+  const meeting = async (element) => {
+    const appID = 2031079141;
+    const serverSecret = "3462328e7fc96f59890af74009f8f2d1";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      Date.now().toString(),
+      "Gaboy2k1"
+    );
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+
+    zp.joinRoom({
+      container: element,
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall,
+      },
+    });
+  };
+
+  return <div ref={meeting} style={{ width: "100vw", height: "100vh" }}></div>;
+};
+
+export default Room;
